8adcd24f170f9bdeeaeb6e2f074cd42597b9f0a8
Dan Stowell <danstowell@users.sourceforge.net>
2010-09-05 08:48:37 +0100

    view now updates note location (randomly, for now)

--- src/uk/co/mcld/dabble/bumbletab/BumbleTab.java
+++ src/uk/co/mcld/dabble/bumbletab/BumbleTab.java
@@ -2,10 +2,12 @@ package uk.co.mcld.dabble.bumbletab;
 
 import android.app.Activity;
 import android.os.Bundle;
+import java.util.Random;
 
 public class BumbleTab extends Activity {
 	
 	private GtrTabView gtrTabView;
+	private TabUpdateThread tabUpdateThread;
 	
     /** Called when the activity is first created. */
     @Override
@@ -19,5 +21,26 @@ public class BumbleTab extends Activity {
 		gtrTabView.timeDivisionPaint.setColor(getResources().getColor(android.R.color.primary_text_light));
 		gtrTabView.usrCircPaint.setColor(getResources().getColor(android.R.color.primary_text_light));
         
+		tabUpdateThread = new TabUpdateThread();
+		tabUpdateThread.start();
     }
+
+    private class TabUpdateThread extends Thread {
+    	public void run(){
+    		Random generator = new Random();
+    		for(int i=0; i < 100; i++){
+    			try {
+    				Thread.sleep(3000L);
+    			} catch (InterruptedException e) {
+    				// TODO Auto-generated catch block
+    				e.printStackTrace();
+    			}
+    			
+    			gtrTabView.whichStrg = generator.nextInt(6);
+    			gtrTabView.whichFret = generator.nextInt(10);
+    			gtrTabView.postInvalidate();
+    	    }
+    	}
+    }
+
 }
